a9631bef544a063245c2769590b2a84ac4d70ed9
sameer47011 <sameer47011@gmail.com>
2024-10-22 11:17:25 +0530

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-
-// import './App.css'
-// import About from './components/About'
-// import Contact from './components/Contact'
-// import Footer from './components/Footer'
-// import Hero from './components/Hero'
-// import Navbar from './components/Navbar'
-// import Projects from './components/Projects'
-// import Service from './components/Service'
-
-// function App() {
-
-//   return (
-//     <div>
-//         <Navbar />
-//         <Hero />
-//         <About />
-//         <Service />
-//         <Projects />
-//         <Contact />
-//         <Footer />
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
